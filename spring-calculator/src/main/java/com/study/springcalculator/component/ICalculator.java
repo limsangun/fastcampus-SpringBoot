815d1366191014f0dcdfe556943b726a1260d518
SangUn <tkrnfldlekd@gmail.com>
2023-02-13 09:13:48 +0900

    junit test

---
+++ spring-calculator/src/main/java/com/study/springcalculator/component/ICalculator.java
@@ -0,0 +1,12 @@
+package com.study.springcalculator.component;
+
+public interface ICalculator {
+
+	int sum(int x, int y);
+	
+	int minus(int x, int y);
+	
+	void init();
+	
+	
+}
---
+++ spring-calculator/src/test/java/com/study/springcalculator/SpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
